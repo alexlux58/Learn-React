f027c83165a9377b4288ad31a8f16fa0f2f1d01a
Alex Lux <alexlux@live.com>
2023-06-27 23:15:31 -0700

    working with components

---
+++ React-Basics/src/App.js
@@ -0,0 +1,36 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+  // Imparative Approach
+  // const para = document.createElement("p");
+  // para.textContent = "This is also visible";
+  // document.getElementById("root").append(para);
+
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 250,
+      date: new Date(2023, 6, 18),
+    },
+    {
+      title: "Car Insurance",
+      amount: 250,
+      date: new Date(2023, 6, 18),
+    },
+    {
+      title: "Car Insurance",
+      amount: 250,
+      date: new Date(2023, 6, 18),
+    },
+  ];
+
+  return (
+    <div>
+      {/* <h2>Let's get started!</h2> */}
+      <Expenses expenses={expenses} />
+      {/* <p>This is not also visible</p> */}
+    </div>
+  );
+}
+
+export default App;
